b0895524bc10cece3c9c6f7e8c32b55d5e2c9e6b
LukasdeSouza <lucas@bestdeal.com.br>
2023-10-15 15:30:52 -0300

    fix: correções no menu header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -8,16 +8,16 @@ const HeaderComponent = () => {
     'PROJETOS',
     'GALERIA',
     'CONTATO',
-    <button className='bg-orange-600 text-white mr-8'>
+    <button className='bg-orange-500 text-white mr-8 p-1 rounded-md'>
       FAZER DOAÇÃO
     </button>
   ]
 
   return (
     <nav>
-      <ul className='w-full my-3 flex justify-between border-r-1 border-r-gray-500'>
+      <ul className='w-full my-3 flex justify-between'>
         {menuItems.map((item) => (
-          <li>{item}</li>
+          <li className="font-medium border-r-2 border-gray-200 border-spacing-x-2">{item}</li>
         ))}
       </ul>
     </nav>
